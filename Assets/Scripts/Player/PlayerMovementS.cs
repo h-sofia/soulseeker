af6c552106a8ad9f006b7957f8db54310e974507
DESKTOP-6FK2FIO\paois <paoisacaliendo@gmail.com>
2026-08-29 18:01:32 -0600

    Player Movementsss

---
+++ Assets/Scripts/Player/PlayerMovementS.cs
@@ -0,0 +1,38 @@
+using UnityEngine;
+
+public class PlayerMovementS : MonoBehaviour
+{
+    [SerializeField] private float _moveSpeed = 5f;
+
+    private Vector2 _movement;
+
+    private Rigidbody2D _rb;
+    private Animator _animator;
+
+    private const string _horizontal = "Horizontal";
+    private const string _vertical = "Vertical";
+    private const string _lastHorizontal = "LastHorizontal";
+    private const string _lastVertical = "LastVertical";
+
+    private void Awake()
+    {
+        _rb = GetComponent<Rigidbody2D>();
+        _animator = GetComponent<Animator>();
+    }
+
+    private void Update()
+    {
+        _movement.Set(INPUTMANAGER.Movement.x, INPUTMANAGER.Movement.y);
+
+        _rb.linearVelocity = _movement * _moveSpeed;
+
+        _animator.SetFloat(_horizontal, _movement.x);
+        _animator.SetFloat(_vertical, _movement.y);
+
+        if (_movement != Vector2.zero)
+        {
+            _animator.SetFloat(_lastHorizontal, _movement.x);
+            _animator.SetFloat(_lastVertical, _movement.y);
+        }
+    }
+}
